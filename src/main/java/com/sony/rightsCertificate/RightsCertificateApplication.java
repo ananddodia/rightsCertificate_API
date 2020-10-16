0bb9caa450ac08773d93cf2e8d52d377fc978a72
ananddodia <aanand.dodia@gmail.com>
2020-10-16 12:56:09 +0530

    initial commit

---
+++ src/main/java/com/sony/rightsCertificate/RightsCertificateApplication.java
@@ -0,0 +1,13 @@
+package com.sony.rightsCertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightsCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightsCertificateApplication.class, args);
+	}
+
+}
